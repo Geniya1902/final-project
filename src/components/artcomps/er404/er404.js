d2ce485c3adcb839aef7cb694776974bf451827d
Евгения <egeshka1902@gmail.com>
2024-05-12 14:35:56 +0300

    about

--- src/components/artcomps/er404/er404.js
+++ src/components/artcomps/er404/er404.js
@@ -1,3 +1,4 @@
+import Finance from '../../../media/finance.png'
 import React from "react";
 import './er404.css'
 
@@ -5,7 +6,7 @@ export default function Er404() {
     return (
         <div className="er404">
             <p>Давайте погрузимся в мир финансовой грамотности и цифр....</p>
-            <img className="finance" src="https://i.pinimg.com/736x/4a/f1/b0/4af1b0cf9bd9d512add99695c5406453.jpg" />
+            <img className="finance" src={Finance} />
         </div>
     );
 }
